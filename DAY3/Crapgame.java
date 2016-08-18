02592b19efc40b30900555fa2d998c476d9f3f00
Shaquil Timmons <djshaq1@yahoo.com>
2016-08-18 16:59:18 -0400

    updated

--- DAY3/Crapgame.java
+++ DAY3/Crapgame.java
@@ -3,51 +3,54 @@ import java.util.Random;
 public class Crapgame {
 	int totalWins = 0;
 	int totalLosses = 0;
+	boolean forward = true;
 public void run(){ 
-	int count = 1;
-	boolean isFinished = false;
 	
-	int point = 0;
 	Random rn = new Random();
 	int dice = 0;
 	dice = (rn.nextInt(6) + 1) + (rn.nextInt(6) + 1);
-
-	while (isFinished == false) {
-		if (count == 1) {
+	System.out.printf("You rolled a %d\n", dice);
 			if (dice == 12 || dice == 2 || dice == 3) {
 				totalLosses++;
-				isFinished = true;
+				System.out.println("You Lose ");
+				
 
 			} else if (dice == 7) {
 				totalWins++;
-				isFinished = true;
-
+				System.out.println("You Win ");
+				
 			} else {
-				point = dice;
+				int point = dice;
+				System.out.printf("Point is %d\n", point);
+				while(forward = true) {
 				dice = (int) (rn.nextInt(6) + 1) + (int) (rn.nextInt(6) + 1);
-
-			}
-
-		} else {
+				
+			
+			System.out.printf("Next roll %d\n", dice);
 			if (dice == point) {
 				totalWins++;
-
-				isFinished = true;
-			} else if (dice == 7) {
+			System.out.println("You Win ");
+				break;
+			}  if (dice == 7) {
 				totalLosses++;
+			System.out.println("You Lose ");
+				break; 
+				
+			
 			}
 
 		}
-	}
-	
+
+			}
 }
 
 	public static void main(String[] args) {
 		Crapgame myGame = new Crapgame();
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 5; i++) {
 		myGame.run();
 		}
 		System.out.printf("The total wins for this game is %d\n", myGame.totalWins);
 		System.out.printf("The total losses for this game is %d\n", myGame.totalLosses);
+		System.out.printf("Do you want to play again? ");
 	}
 }
