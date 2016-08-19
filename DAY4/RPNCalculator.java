8d1fb789d4d4fdd5b3659b45f33032cb281a1d7b
Shaquil Timmons <djshaq1@yahoo.com>
2016-08-19 11:03:56 -0400

    updated

--- DAY4/RPNCalculator.java
+++ DAY4/RPNCalculator.java
@@ -4,8 +4,8 @@ import java.util.Stack;
 
 public class RPNCalculator {
 	public static void main(String[] args) {
-		Stack<Integer> results = new Stack<Integer>();
-		char[] problem = new char[] { '1', '2', '+', '6', '+' };
+		Stack<Integer> results = new Stack<Integer>(); // creation of stack. named it results
+		char[] problem = new char[] { '8', '2', '/', '9', '*','1','-','7','/' };
 		for (int i = 0; i < problem.length; i++) {
 			char ch = problem[i];
 
@@ -14,18 +14,20 @@ public class RPNCalculator {
 			if (val != -1) {
 				results.push(val);
 			} else {
+				int p2 = (int)results.pop();
+				int p1 = (int)results.pop();
 				switch (ch) {
 				case '+':
-					results.push(results.pop() + results.pop());
+					results.push(p1 + p2);
 					break;
 				case '-':
-					results.push(results.pop() - results.pop());
+					results.push(p1 - p2);
 					break;
 				case '*':
-					results.push(results.pop() * results.pop());
+					results.push(p1 * p2);
 					break;
 				case '/':
-					results.push(results.pop() / results.pop());
+					results.push(p1 / p2);
 					break;
 				}
 			}
